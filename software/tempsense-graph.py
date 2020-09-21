53fb99e80ef8b39c42f4ab178ea9f98c287475d9
Daniel Soto <sotod@sonoma.edu>
2020-09-21 13:02:56 -0700

    edits to enhance readability

--- software/tempsense-graph.py
+++ software/tempsense-graph.py
@@ -1,24 +1,18 @@
-import time
-import board
-import busio
-import digitalio
-import adafruit_max31855
+import board                  # this library defines the metal pins on the board
+import busio                  # this library allows us to speak SPI
+import digitalio              # this library lets us turn metal pins on and off
+import adafruit_max31855      # this library know how to talk to the temperature board
+import datetime               # this library knows the clock and the calendar
 
-import datetime
-
+# library to make plot and data source
 from bokeh.plotting import figure, ColumnDataSource
+# library to place graph on webpage
 from bokeh.io import curdoc
 
-# TODO: improve variable names where possible
-# TODO: add required concepts (arrays, dictionaries)
+# This script uses arrays, dictionaries, date and time formatting codes (strftime) and callback functions.
+# Search for these python concepts online to understand any unfamiliar syntax.
 
-# initalizations
-
-log_interval_ms = 5E3
-
-# maybe don't use arrays here either if not necessary
-# continue plotting to screen
-
+# this function returns an array of sensor objects that can be handled as a unit
 def config_sensors():
     spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
     cs = [digitalio.DigitalInOut(board.D4),
@@ -30,32 +24,30 @@ def config_sensors():
             adafruit_max31855.MAX31855(spi, cs[2]),
             adafruit_max31855.MAX31855(spi, cs[3])]
 
+# this function reads the temperature of each of thermocouples
 def read_temps(sensors):
     return [sensors[0].temperature,
             sensors[1].temperature,
             sensors[2].temperature,
             sensors[3].temperature]
 
+# this function writes each of the temperatures to a line in a logging file
 def write_temps(log_file, temps, sample_time):
     with open(log_file, 'a') as f:
         f.write(sample_time.strftime('%Y-%m-%dT%H:%M:%S'))
-        #f.write(',')
         for temp in temps:
             f.write(',{}'.format(temp))
         f.write('\n')
 
-sensors = config_sensors()
-
 
 # this is where we define our graphs and connect them to our data
-def make_document(doc):
+def make_document(webpage):
 
     # this is where we read our data and place it in the array for the graph data
     # this function is called by doc.add_periodic_callback
     def update():
 
         # read temperatures and place in column data store
-        # TODO: make this more verbose?
         temps = read_temps(sensors)
         sample_time = datetime.datetime.now()
         write_temps(log_file, temps, sample_time)
@@ -63,8 +55,7 @@ def make_document(doc):
         # adds a measurement to the graphing temperatures
         # this adds a row to the table
         # .stream adds a row of data to this table
-        # TODO: change this name to graph_data
-        temp_source.stream({'time': [sample_time],
+        graph_data.stream({'time': [sample_time],
                             'temp0': [temps[0]],
                             'temp1': [temps[1]],
                             'temp2': [temps[2]],
@@ -72,54 +63,58 @@ def make_document(doc):
 
     # special place to hold temperatures for graphing
     # we can think of this as a table with columns
-    temp_source = ColumnDataSource({'time': [],
-                                    'temp0': [],
-                                    'temp1': [],
-                                    'temp2': [],
-                                    'temp3': []})
+    graph_data = ColumnDataSource({'time': [],
+                                   'temp0': [],
+                                   'temp1': [],
+                                   'temp2': [],
+                                   'temp3': []})
 
-    # TODO: make this plot set up stuff flow if possible
-    # TODO: make document, make plot, set up update, then add lines and shapes
 
-    # tells the computer to update the graph at a regular time interval
-    doc.add_periodic_callback(update, log_interval_ms)
-
-    # creates a plot
-    temp_fig = figure(title = '', x_axis_type='datetime')
-    temp_fig.xaxis.axis_label = 'Time'
-    temp_fig.yaxis.axis_label = 'Temperature (C)'
-
+    # creates a plot figure, labels axes, and sets x-axis as time
+    graph = figure(title = '', x_axis_type='datetime')
+    graph.xaxis.axis_label = 'Time'
+    graph.yaxis.axis_label = 'Temperature (C)'
 
     # create lines for each temperature measurement
-    # do we want lines at all?  too much complexity?
-    # each line uses data from our temperature table above
-    temp_fig.line(source=temp_source, x='time', y='temp0', legend_label='1 (CS5)', line_width=1, color='blue')
-    temp_fig.line(source=temp_source, x='time', y='temp1', legend_label='2 (CS6)', line_width=1, color='green')
-    temp_fig.line(source=temp_source, x='time', y='temp2', legend_label='3 (CS7)', line_width=1, color='red')
-    temp_fig.line(source=temp_source, x='time', y='temp3', legend_label='4 (CS8)', line_width=1, color='black')
+    # each line uses data from 'graph_data' above
+    # the x and y labels match the labels in 'graph_data'
+    graph.line(source=graph_data, x='time', y='temp0', legend_label='1 (CS5)', line_width=1, color='blue')
+    graph.line(source=graph_data, x='time', y='temp1', legend_label='2 (CS6)', line_width=1, color='green')
+    graph.line(source=graph_data, x='time', y='temp2', legend_label='3 (CS7)', line_width=1, color='red')
+    graph.line(source=graph_data, x='time', y='temp3', legend_label='4 (CS8)', line_width=1, color='black')
 
     # create shapes and colors and sizes for each temperature measurement
-    temp_fig.circle(source=temp_source, x='time', y='temp0', size=4, color='blue')
-    temp_fig.circle(source=temp_source, x='time', y='temp1', size=4, color='green')
-    temp_fig.circle(source=temp_source, x='time', y='temp2', size=4, color='red')
-    temp_fig.circle(source=temp_source, x='time', y='temp3', size=4, color='black')
+    graph.circle(source=graph_data, x='time', y='temp0', size=4, color='blue')
+    graph.circle(source=graph_data, x='time', y='temp1', size=4, color='green')
+    graph.circle(source=graph_data, x='time', y='temp2', size=4, color='red')
+    graph.circle(source=graph_data, x='time', y='temp3', size=4, color='black')
 
-    temp_fig.legend.location = 'top_left'
-    print(type(temp_fig.legend))
+    # set the legend location on the graph
+    graph.legend.location = 'top_left'
 
-    # set up the figure and the plot
-    plots = temp_fig
-    doc.add_root(plots)
-    doc.title = "Temperature Plotting"
+    # add the graph to a web page and call update periodically
+    webpage.add_root(graph)
+    webpage.title = "Temperature Plotting"
+    # tells the computer to update the graph by calling the "update" function at a regular time interval
+    webpage.add_periodic_callback(update, log_interval_ms)
 
-print('starting')
+# below this line are the commands executed by the script
+# (above this we defined the functions, but did not run them)
 
-# TODO: how do you make this cleaner for first-time readers
+# create an array of temperature sensor objects
+sensors = config_sensors()
+# set the time between temperature readings in milliseconds
+log_interval_ms = 5000
+
+# creates a filename with the current date and time and writes the header line to the file
 log_file = datetime.datetime.now().strftime('%Y-%m-%d-%H%M%S') + '-log.csv'
 with open(log_file, 'w') as f:
     f.write('time,temp_1_C,temp_2_C,temp_3_C,temp_4_C\n')
 
+# send messages to the output console
+print('Starting temperature instrument')
 print('logging to file {}'.format(log_file))
 
-# this is a mysterious command that creates the webpage and the plot within it
+# this command creates the webpage and the plot within it by running the 'make_document' function above
+# note curdoc() is called webpage in 'make_document'
 make_document(curdoc())
