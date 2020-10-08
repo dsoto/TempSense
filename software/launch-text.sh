dba008089cbcca09892dd18a4ddadd12ddb8f44c
Daniel Soto <sotod@sonoma.edu>
2020-10-08 11:37:12 -0700

    updates scripts

--- software/launch-text.sh
+++ software/launch-text.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # create environment variable
-# set BLINKA_FT232H=1    # command for windows
+# set BLINKA_FT232H=1    # alternate command for windows
 export BLINKA_FT232H=1   # command for linux and os x
 
 # activate python custom library
+# source activate tempsense  # alternate command
 conda activate tempsense
 
 # run text output program in python
